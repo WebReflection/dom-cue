d666f32979c0b2842bd426908764f517e640109a
webreflection <andrea.giammarchi@gmail.com>
2025-10-15 18:34:43 +0200

    Added a pure Python version of the same logic

---
+++ src/signals.py
@@ -0,0 +1,237 @@
+__all__ = ['Signal', 'signal', 'Computed', 'computed', 'effect', 'batch', 'untracked']
+
+
+class Set(list):
+    """
+    Strictly mimicking the JS Set class
+    allowing operations otherwise hard to obtain
+    with just the list class (such as remove(item))
+    """
+
+    def add(self, value):
+        for item in self:
+            if item is value:
+                return self
+
+        self.append(value)
+
+        return self
+
+    def delete(self, value):
+        i = -1
+
+        for item in self:
+            i += 1
+
+            if item is value:
+                break
+
+        if i != -1:
+          self.pop(i)
+
+
+def cleared(self):
+    """
+    Clears the set and returns a copy of the items
+    """
+    computed = list(self)
+    self.clear()
+    return computed
+
+
+# used to store computed signals that need to be updated
+batched = Set()
+
+# whether the current computation is synchronous (not batched)
+synchronous = True
+
+# whether the current computation is being tracked
+tracked = True
+
+# the current computed signal used to subscribe
+computing = None
+
+
+class Signal(Set):
+    """
+    A signal is a value that can be subscribed to and notified when it changes.
+    """
+
+    def __init__(self, value):
+        super().__init__()
+        self._value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f"Signal({self._value})"
+
+    @property
+    def value(self):
+        if tracked and computing is not None:
+            self.add(computing.add(self))
+
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if self._value is not value:
+            self._value = value
+            for computed in cleared(self):
+                computed._update()
+
+    def peek(self):
+        return self._value
+
+
+# create a signal with the given value
+signal = lambda value: Signal(value)
+
+
+class Computed(Signal):
+    """
+    A computed signal is a read-only signal that is computed from other signals.
+    """
+
+    def __init__(self, value, fx=False):
+        super().__init__(value)
+        self._compute = True
+        self._computed = None
+        self._subscribe = not fx
+
+    def __repr__(self):
+        return f"Computed({self._computed})"
+
+    def _run(self):
+        global computing
+
+        if (self._compute):
+            previously = computing
+            computing = self
+            self._compute = False
+            self.clear()
+            try:
+                self._computed = self._value()
+            finally:
+                computing = previously
+
+    def _update(self):
+        self._compute = True
+
+        if synchronous:
+            self._subscribe or self._run()
+        else:
+            batched.add(self)
+
+    @property
+    def value(self):
+        self._run()
+
+        if self._subscribe and tracked and computing is not None:
+            for signal in cleared(self):
+                signal.add(computing.add(signal))
+
+        return self._computed
+
+    def peek(self):
+        self._run()
+        return self._computed
+
+
+# create a computed signal via the given getter
+computed = lambda value: Computed(value)
+
+
+def effect(callback):
+    """
+    An effect is a callback that is automatically called when its signals change.
+    """
+
+    value = None
+
+    def effect():
+        nonlocal value
+
+        if callable(value):
+            value()
+
+        value = callback()
+
+    def cleanup():
+        for signal in cleared(fx):
+            signal.delete(fx)
+
+        if callable(value):
+            value()
+
+    fx = Computed(effect, fx=True)
+    fx.value
+
+    return cleanup
+
+
+def batch(callback):
+    """
+    Run the callback without tracking its signals.
+    """
+
+    global synchronous
+
+    finalize = synchronous
+    synchronous = False
+
+    try:
+        callback()
+        if finalize and len(batched):
+            synchronous = finalize
+
+            for batch in cleared(batched):
+                if batch._compute:
+                    batch._update()
+
+    finally:
+        synchronous = finalize
+
+
+def untracked(callback):
+    """
+    Run the callback without tracking its signals.
+    """
+    global tracked
+
+    tracking = tracked
+    tracked = False
+    try:
+        return callback()
+    finally:
+        tracked = tracking
+
+
+
+# # test
+
+# s = signal(1)
+# c = computed(lambda: s.value * 2)
+
+# assert c.value == 2
+# s.value += 1
+# assert c.value == 4
+
+# @effect
+# def counter():
+#     print('effect', c.value)
+#     return lambda: print("unmounted")
+
+
+# @batch
+# def increment():
+#     s.value += 1
+#     s.value += 1
+
+# assert c.value == 8
+
+# counter()
+
+# s.value += 1
+# assert c.value == 10
